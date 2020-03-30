2714591f7b69c9ac3ddad3d3be3fbd846e5687c2
Sandra Navarro <lainfaith2@gmail.com>
2020-03-30 12:27:00 +0200

    Added modules and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, APP_ROUTES } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './ui/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
-import { HeaderComponent } from './ui/header/header.component';
-import { SidebarComponent } from './ui/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './ui/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register/register.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphs1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
